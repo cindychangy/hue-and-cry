2e0d0f0708ea1ab2a1d32069903c817f0796e15d
cindychangy <cindyisachang@gmail.com>
2021-01-20 19:52:20 +0100

    fixing head doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,25 @@
-import React from "react";
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-class Document extends NextDocument {
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap"
-            rel="stylesheet"
-          ></link>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default Document;
+export default MyDocument
