ee0a20852a2f4cbc3748bb2a171fb92b88856d8e
cindychangy <cindyisachang@gmail.com>
2021-04-17 12:13:21 +0200

    updating head tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,8 +16,18 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
     <Head>
-      <title>The Hue and Cry : True Crime Blog</title>
+      <title>The Hue and Cry | True Crime Blog</title>
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <meta name="description" content="A true crime blog focusing on crimes against women and girls who have yet to see justice." />
+      <link rel="canonical" href="https://thehueandcry.com/" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Hue and Cry | True Crime Blog" />
+      <meta property="og:description" content="A true crime blog focusing on crimes against women and girls who have yet to see justice." />
+      <meta property="og:url" content="https://thehueandcry.com/" />
+      <meta property="og:site_name" content="Hue and Cry" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@thehue_andcry" />
     </Head>
     <ThemeProvider theme={theme}>
       <CssBaseline />
