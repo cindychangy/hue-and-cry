4e4e6e3107554854e541a0f2684cd068d415228d
cindychangy <cindyisachang@gmail.com>
2021-04-19 08:28:20 +0200

    updating about box padding

--- src/app/home/sidebar/Sidebar.styles.ts
+++ src/app/home/sidebar/Sidebar.styles.ts
@@ -39,7 +39,7 @@ export const useStyles = makeStyles(({ spacing, breakpoints, palette }) => ({
     },
     '& > span:not(:first-child)': {
       marginLeft: 0,
-      [breakpoints.down('lg')]: {
+      [breakpoints.down('md')]: {
         marginLeft: spacing(0.6),
       },
     }
