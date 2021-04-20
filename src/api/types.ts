6aef5c3d69d5aa18f70a68440bb64d40272b4b1e
cindychangy <cindyisachang@gmail.com>
2021-04-20 19:13:42 +0200

    changing API value

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,4 @@
-export const siteURL = 'https://thehueandcry.com/wp-json';
+export const siteURL = 'https://wordpress.thehueandcry.com/wp-json';
 
 export * from './actions/posts/posts.types';
 export * from './actions/categories/categories.types';
