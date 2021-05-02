1064689c3c0d79f782d9fedf50219364395ed26d
cindychangy <cindyisachang@gmail.com>
2021-05-02 16:45:32 +0200

    add lazyload to homepage cta banner

--- src/app/home/ctaBanner/CtaBanner.tsx
+++ src/app/home/ctaBanner/CtaBanner.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
+import LazyLoad from 'react-lazyload';
 
-import { Box, Typography, Link } from 'components/atoms'
+import { Box, Typography, Link } from 'components/atoms';
 
 import { AppRoute } from 'app/App.types';
 import { useStyles } from './CtaBanner.styles';
@@ -16,19 +17,21 @@ export const CtaBanner = () => {
   }
 
   return (
-    <Box className={classes.ctaContainer} style={featureImage}>
-      <Box className={classes.ctaTextWrapper}>
-        <Typography variant="h2" className={classes.ctaHeading}>
-          Highway of Tears
-        </Typography>
-        <Typography className={classes.ctaText}>
-          The Highway of Tears is an isolated 725-kilometer stretch of Highway 16 between Prince George and Prince Rupert in British Columbia, Canada. This highway has been the location of many unsolved murders and disappearances of Indigenous women beginning as early as the 1970s.
-        </Typography>
-        <Typography variant="h5">
-        <Link href={AppRoute.HIGHWAY_OF_TEARS} className={classes.seeMore}>See all stories</Link>
-        </Typography>
+    <LazyLoad height={250} once>
+      <Box className={classes.ctaContainer} style={featureImage}>
+        <Box className={classes.ctaTextWrapper}>
+          <Typography variant="h2" className={classes.ctaHeading}>
+            Highway of Tears
+          </Typography>
+          <Typography className={classes.ctaText}>
+            The Highway of Tears is an isolated 725-kilometer stretch of Highway 16 between Prince George and Prince Rupert in British Columbia, Canada. This highway has been the location of many unsolved murders and disappearances of Indigenous women beginning as early as the 1970s.
+          </Typography>
+          <Typography variant="h5">
+          <Link href={AppRoute.HIGHWAY_OF_TEARS} className={classes.seeMore}>See all stories</Link>
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </LazyLoad>
   )
 };
 
