c71d79a0892e32914c5e14c7482b6e6141fd3d87
cindychangy <cindyisachang@gmail.com>
2022-02-28 20:36:11 +0100

    adding disclaimer text

--- src/app/highwayOfTears/HighwayOfTears.tsx
+++ src/app/highwayOfTears/HighwayOfTears.tsx
@@ -61,10 +61,13 @@ export const HighwayOfTearsHome = () => {
               The Highway of Tears is an isolated 725-kilometer stretch of Highway 16 between Prince George and Prince Rupert in British Columbia, Canada.
             </S.BoxHeader>
             <S.ParagraphStyled>
-              It has been the location of numerous murders and disappearances beginning as early as the late 60's. The majority of the victims belong to Indiginous communities. Poverty rates in this area are high and hitchhiking is often the only way for many to travel to work, school, visit family, or seek medical treatment. 
+              It has been the location of numerous murders and disappearances starting in the late 60's. The majority of the victims belong to Indiginous communities. Poverty rates in this area are high and hitchhiking is often the only way for many to travel to work, school, visit family, or seek medical treatment. 
             </S.ParagraphStyled>
             <S.ParagraphStyled>
               Historically, Indigenous communities have been plagued by systematic racism and negligance from authorities, which is reflected in how many of these cases were handled. It is a widely believed that these cases will never be solved, but the families of the victims that were affected, and the victims themselves, deserve to have their stories heard and remembered.
+            </S.ParagraphStyled>
+            <S.ParagraphStyled sx={{ fontWeight: 900 }}>
+              This timeline highlights the unsolved cases of The Highway of Tears.
             </S.ParagraphStyled>
           </S.BoxGray>
         </Grid>
