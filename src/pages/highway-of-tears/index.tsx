db9177b063c4374561d56157ae356d1246cb1957
cindychangy <cindyisachang@gmail.com>
2022-03-05 11:59:17 +0100

    update twitter image for highway of tears

--- src/pages/highway-of-tears/index.tsx
+++ src/pages/highway-of-tears/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head';
 import { HighwayOfTearsHome } from '../../app/highwayOfTears/HighwayOfTears';
-import { baseURL, metaImage } from 'api/types';
+import { baseURL } from 'api/types';
 
 const HighwayOfTearsPage = () => {
 
@@ -16,7 +16,7 @@ const HighwayOfTearsPage = () => {
         <meta property="og:description" content="The Highway of Tears is an isolated 725-kilometer stretch of Highway 16. It has been the location of many unsolved murders and disappearances of Indigenous women." />
         <meta property="og:url" content={`${baseURL}/highway-of-tears`} />
         <meta property="og:site_name" content="Hue and Cry" />
-        <meta property="og:image" content={metaImage} />
+        <meta property="og:image" content="https://wordpress.thehueandcry.com/wp-content/uploads/highwayoftears.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@thehueandcry" />
         <meta name="twitter:site" content="@thehueandcry" />
