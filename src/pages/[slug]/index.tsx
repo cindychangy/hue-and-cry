425b890d7ae6752028a204e4775f2e32391e2841
cindychangy <cindyisachang@gmail.com>
2022-05-24 14:16:19 +0200

    fix issue with cta help text

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -46,7 +46,7 @@ export const getStaticProps: GetStaticProps = async ( { params }) => {
             digDeeper
           }
           ctaHowToHelp {
-            fieldGroupName
+            howToHelp
           }
           featuredImage {
             node {
