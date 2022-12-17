741faa44fade4cd642dacb2e39ac340400035a3f
Cindy Chang <cindyisachang@gmail.com>
2022-12-17 13:30:58 -0800

    upgrade to next 13

--- src/app/highwayOfTears/HighwayOfTears.tsx
+++ src/app/highwayOfTears/HighwayOfTears.tsx
@@ -14,7 +14,6 @@ import { TIMELINE_DATA } from 'constants/timeline';
 import * as S from './HighwayOfTears.styles';
 
 export const HighwayOfTearsHome = () => {
-
   const timelineList = TIMELINE_DATA.map((data) => (
     <S.TimelineCase key={data.name}>
       <S.CaseMeta>
@@ -31,18 +30,26 @@ export const HighwayOfTearsHome = () => {
             <S.TimelineName>{data.name}</S.TimelineName>
             <Typography>
               {data.blurb}
-              {!!data.link && <><S.TimelineLink href={data.link}> Read full story</S.TimelineLink>.</>}
+              {!!data.link && (
+                <>
+                  <S.TimelineLink href={data.link}>
+                    {' '}
+                    Read full story
+                  </S.TimelineLink>
+                  .
+                </>
+              )}
             </Typography>
           </Box>
-          {data.photo && 
-            <S.ImageContainer 
-            sx={{ 
-              background: `url(${data.photo})`, 
-              backgroundSize: 'cover', 
-              backgroundPosition: 'center' 
-            }}>
-            </S.ImageContainer>
-          }
+          {data.photo && (
+            <S.ImageContainer
+              sx={{
+                background: `url(${data.photo})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            ></S.ImageContainer>
+          )}
         </S.TimelineBio>
       </TimelineContent>
     </S.TimelineCase>
@@ -51,31 +58,44 @@ export const HighwayOfTearsHome = () => {
   return (
     <Page>
       <SectionHeading />
-      <Box mb={4}/>
+      <Box mb={4} />
       <Grid container>
-        <Grid item xs={12}>
-        </Grid>
+        <Grid item xs={12}></Grid>
         <Grid item xs={12} lg={4}>
           <S.Title>Highway of Tears</S.Title>
           <S.BoxGray>
             <S.BoxHeader>
-              The Highway of Tears is an isolated 725-kilometer stretch of Highway 16 between Prince George and Prince Rupert in British Columbia, Canada.
+              The Highway of Tears is an isolated 725-kilometer stretch of
+              Highway 16 between Prince George and Prince Rupert in British
+              Columbia, Canada.
             </S.BoxHeader>
             <S.ParagraphStyled>
-              It has been the location of numerous murders and disappearances starting in the late 60's. The majority of the victims belong to Indiginous communities. Poverty rates in this area are high and hitchhiking is often the only way for many to travel to work, school, visit family, or seek medical treatment. 
+              It has been the location of numerous murders and disappearances
+              starting in the late 60's. The majority of the victims belong to
+              Indiginous communities. Poverty rates in this area are high and
+              hitchhiking is often the only way for many to travel to work,
+              school, visit family, or seek medical treatment.
             </S.ParagraphStyled>
             <S.ParagraphStyled>
-              Historically, Indigenous communities have been plagued by systematic racism and negligance from authorities, which is reflected in how many of these cases were handled. It is a widely believed that these cases will never be solved, but the families of the victims that were affected, and the victims themselves, deserve to have their stories heard and remembered.
+              Historically, Indigenous communities have been plagued by
+              systematic racism and negligance from authorities, which is
+              reflected in how many of these cases were handled. It is a widely
+              believed that these cases will never be solved, but the families
+              of the victims that were affected, and the victims themselves,
+              deserve to have their stories heard and remembered.
             </S.ParagraphStyled>
             <S.SubTextStyled>
-             <span>This timeline highlights the unsolved cases of The Highway of Tears. <ArrowForwardIcon/></span>
+              <span>
+                This timeline highlights the unsolved cases of The Highway of
+                Tears. <ArrowForwardIcon />
+              </span>
             </S.SubTextStyled>
           </S.BoxGray>
         </Grid>
         <Grid item xs={12} lg={8}>
-          <Timeline>
+          {/* <Timeline>
             {timelineList}
-          </Timeline>
+          </Timeline> */}
         </Grid>
       </Grid>
     </Page>
