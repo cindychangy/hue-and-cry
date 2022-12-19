d897d4ef22663564bbfa9df00f3ef8fa285ee9ed
Cindy Chang <cindyisachang@gmail.com>
2022-12-18 20:29:18 -0800

    update scaffolding

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     appDir: true,
   },
-  images: {
-    domains: ['i0.wp.com', 'wordpress.thehueandcry.com'],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+
