ab5b8f42fe040ac50e5a8174910e7478e261aa93
Cindy Chang <cindyisachang@gmail.com>
2022-12-18 20:59:02 -0800

    add lint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
-'use client';
-import React from 'react';
+'use client'
+import React from 'react'
 
-import Link from 'next/link';
+import Link from 'next/link'
 export default function Home() {
-  return (
-    // <Grid
-    //   container
-    //   height="100vh"
-    //   alignItems="center"
-    //   justifyContent="center"
-    //   direction="column"
-    // >
-    //   <h1>Using Material UI with Next.js 13</h1>
-    //   <Stack direction="row" columnGap={1}>
-    //     <Button variant="text">Text</Button>
-    //     <Button variant="contained">Contained</Button>
-    //     <Button variant="outlined">Outlined</Button>
-    //   </Stack>
-    // </Grid>
-    <Link href="/dashboard">Dashboard</Link>
-  );
+	return (
+		// <Grid
+		//   container
+		//   height="100vh"
+		//   alignItems="center"
+		//   justifyContent="center"
+		//   direction="column"
+		// >
+		//   <h1>Using Material UI with Next.js 13</h1>
+		//   <Stack direction="row" columnGap={1}>
+		//     <Button variant="text">Text</Button>
+		//     <Button variant="contained">Contained</Button>
+		//     <Button variant="outlined">Outlined</Button>
+		//   </Stack>
+		// </Grid>
+		<Link href='/dashboard'>Dashboard</Link>
+	)
 }
