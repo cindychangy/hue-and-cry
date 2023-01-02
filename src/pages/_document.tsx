ab2a809991e177d4e4da9e3dca40fdeed7585c64
Cindy Chang <cindyisachang@gmail.com>
2023-01-02 13:54:20 +0100

    mods to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+	DocumentContext,
+	DocumentInitialProps,
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
+	static async getInitialProps(
+		ctx: DocumentContext
+	): Promise<DocumentInitialProps> {
+		const sheet = new ServerStyleSheet()
+		const originalRenderPage = ctx.renderPage
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				})
+
+			const initialProps = await Document.getInitialProps(ctx)
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			}
+		} finally {
+			sheet.seal()
+		}
+	}
+
 	render() {
 		return (
 			<Html>
@@ -19,7 +55,6 @@ class MyDocument extends Document {
 					></link>
 				</Head>
 				<body>
-					<script>0</script>
 					<Main />
 					<NextScript />
 				</body>
@@ -28,4 +63,34 @@ class MyDocument extends Document {
 	}
 }
 
-export default MyDocument
+// import React from 'react'
+// import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+// class MyDocument extends Document {
+// 	render() {
+// 		return (
+// 			<Html>
+// 				<Head>
+// 					<link
+// 						rel="preconnect"
+// 						href="https://fonts.gstatic.com"
+// 						crossOrigin="anonymous"
+// 					/>
+// 					<link
+// 						href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+// 						rel="stylesheet"
+// 						as="font"
+// 						crossOrigin="anonymous"
+// 					></link>
+// 				</Head>
+// 				<body>
+// 					<script>0</script>
+// 					<Main />
+// 					<NextScript />
+// 				</body>
+// 			</Html>
+// 		)
+// 	}
+// }
+
+// export default MyDocument
