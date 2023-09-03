149df83d302e43839bffe1b52dc5a97d12a88753
Cindy Chang <cindyisachang@gmail.com>
2023-09-03 17:55:25 +0200

    setup google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import Script from 'next/script'
 import StyledComponentsRegistry from '../lib/registry'
 import { ThemeProvider } from 'styled-components'
 import { SiteHeader } from '../components/header'
 import { SiteFooter } from '../components/footer'
 import { theme } from '../styles/theme'
 import GlobalStyles from '../styles/globalStyles'
-
-const GA4_TRACKING_ID = process.env.NEXT_PUBLIC_GA4_TRACKING_ID
+import GoogleAnalytics from '../components/googleAnalytics'
 
 export default function RootLayout({
 	children,
@@ -66,25 +64,12 @@ export default function RootLayout({
 					crossOrigin="anonymous"
 				></link>
 			</head>
-			<Script id="google-tag-manager" strategy="afterInteractive">
-				{`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GA4_TRACKING_ID}');
-        `}
-			</Script>
-			<noscript
-				dangerouslySetInnerHTML={{
-					__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA4_TRACKING_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-				}}
-			/>
 			<body>
 				<StyledComponentsRegistry>
 					<GlobalStyles />
 					<ThemeProvider theme={theme}>
 						<SiteHeader />
+						<GoogleAnalytics />
 						{children}
 						<SiteFooter />
 					</ThemeProvider>
