fe82032617b8b8b373d4b665b436e6a0297d6c08
Cindy Chang <cindyisachang@gmail.com>
2023-10-23 21:31:07 +0200

    configure app types

--- src/constants/navigation.tsx
+++ src/constants/navigation.tsx
@@ -1,4 +1,13 @@
-import { AppRoute } from '../app/app.types'
+enum AppRoute {
+	HOME = '/',
+	ARTICLE = '/:slug',
+	MISSING = '/missing',
+	MURDERED = '/murdered',
+	INDIGENOUS_WOMEN = '/indigenous-women',
+	HIGHWAY_OF_TEARS = '/highway-of-tears',
+	MILITARY = '/military',
+	ABOUT = '/about',
+}
 
 export const NAV_ITEMS = [
 	{ label: 'Missing', value: AppRoute.MISSING },
