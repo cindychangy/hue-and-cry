81c1f6e740911817f8af78d6e33adf3e41e62a66
Cindy Chang <cindyisachang@gmail.com>
2023-10-31 15:27:36 +0100

    fix formatting

--- src/components/googleAnalytics/googleAnalytics.jsx
+++ src/components/googleAnalytics/googleAnalytics.jsx
@@ -1,20 +1,19 @@
 'use client'
 
-import Script from "next/script"
+import Script from 'next/script'
 
 const GoogleAnalytics = () => {
-
-    return (
-        <>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}
-            />
-            <Script
-                id="gtag-init"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
@@ -22,10 +21,10 @@ const GoogleAnalytics = () => {
                       page_path: window.location.pathname,
                       });
                     `,
-                }}
-            />
-        </>
-    )
+				}}
+			/>
+		</>
+	)
 }
 
 export default GoogleAnalytics
