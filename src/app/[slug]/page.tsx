ddedb0b9568ef07af0334fc46e31a53889ee307b
Cindy Chang <cindyisachang@gmail.com>
2023-12-15 15:22:44 +0100

    remove no-cache

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -14,8 +14,6 @@ import {
 } from '@/components'
 import styles from './page.module.css'
 
-export const fetchCache = 'force-no-store'
-
 export async function generateStaticParams() {
 	const { data } = await client.query({
 		query: gql`
