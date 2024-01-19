ec4ea5bdc2e3cedaf7b601d52a6244bb7f66c368
Cindy Chang <cindyisachang@gmail.com>
2024-01-19 10:08:46 +0100

    fix image paths to match new api

--- src/constants/timeline.tsx
+++ src/constants/timeline.tsx
@@ -6,8 +6,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Gloria went out one night with her brother only to disappear behind him on their way home.',
 		link: '/gloria-moody',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/gloria-moody.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/gloria-moody.jpeg`,
 	},
 	{
 		year: '1970',
@@ -15,8 +14,7 @@ export const TIMELINE_DATA = [
 		name: 'Micheline Pare',
 		blurb: 'Micheline was 18 years old when she went missing along Highway 16.',
 		link: '/micheline-pare',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/micheline-pare.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/micheline-pare.png`,
 	},
 	{
 		year: '1970',
@@ -24,8 +22,7 @@ export const TIMELINE_DATA = [
 		name: 'Helen Claire Frost',
 		blurb:
 			'Helen disappeared just four days before her 18th birthday. She was last seen leaving the apartment she shared with her sister, telling her sister she wanted to go for a quick walk. She never returned home.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/helen-claire-frost.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/helen-claire-frost.jpeg`,
 	},
 	{
 		year: '1971',
@@ -34,8 +31,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Jean was last seen hanging out with her cousin on the railroad overpass on Highway 16.',
 		link: '/jean-virginia-sampare',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/Jean_Virginia_Sampare.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Jean_Virginia_Sampare.png`,
 	},
 	{
 		year: '1973',
@@ -44,8 +40,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			"Gale Weys was 19 years old when she was last seen trying to get a ride to her parent's house.",
 		link: '/gale-weys',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/gale-weys-photo.jpg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/gale-weys-photo.jpg`,
 	},
 	{
 		year: '1973',
@@ -53,8 +48,7 @@ export const TIMELINE_DATA = [
 		name: 'Pamela Darlington',
 		blurb:
 			'Pamela was 19 years old when she went missing, she was last seen in a bar in Kamloops. Her body was found the next day in a park. She had been sexually assaulted and beaten to death.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/pamela-photo.jpg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/pamela-photo.jpg`,
 	},
 	{
 		year: '1974',
@@ -63,8 +57,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Monica was 14 years old and was last seen walking along Highway 16 in the direction of her house.',
 		link: '/monica-ignas',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/monica-ignas.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/monica-ignas.png`,
 	},
 	{
 		year: '1974',
@@ -72,8 +65,7 @@ export const TIMELINE_DATA = [
 		name: 'Colleen MacMillen',
 		blurb:
 			"Colleen was 16 years old when she disappeared hitchhiking to a friend's house. Her remains were found one month later. In 2012, with advancements in DNA technology the RCMP announced Bobby Jack Fowler as a suspect in her killing. ",
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/colleen-photo.jpg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/colleen-photo.jpg`,
 	},
 	{
 		year: '1978',
@@ -81,8 +73,7 @@ export const TIMELINE_DATA = [
 		name: 'Mary Jane Hill',
 		blurb:
 			'Mary Jane Hill was 31 years old and a mother of three. Her body was found nude along Highway 16 and her death was deemed to be from bronchitis and bronchopneumonia as a result of manslaughter. ',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/mary-jane-hill.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/mary-jane-hill.png`,
 	},
 	{
 		year: '1989',
@@ -90,8 +81,7 @@ export const TIMELINE_DATA = [
 		name: 'The Jack Family',
 		blurb:
 			'Doreen Jack and her husband Ronald were offered jobs at a logging camp from an unknown man. The struggling family of four accepted the job and ride the man offered. The family was last seen at 1:20am that night leaving with the man in his car.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/doreen-jack.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/doreen-jack.jpeg`,
 	},
 	{
 		year: '1989',
@@ -100,8 +90,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Alberta Williams was 24 years old when she was last seen leaving a bar in Prince Rupert.',
 		link: '/alberta-williams',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/alberta-williams-photo.jpg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/alberta-williams-photo.jpg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '1990',
@@ -110,8 +99,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Delphine Nikal was last seen leaving her group of friends one night to head home, but never made it back.',
 		link: '/delphine-nikal',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/delphine-nikal-poster.jpg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/delphine-nikal-poster.jpg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '1994',
@@ -120,8 +108,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Ramona Wilson was last seen on a Saturday night leaving her house to meet friends for a night out. No one has seen her since.',
 		link: '/ramona-wilson',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/ramona-wilson-photo-1.jpg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/ramona-wilson-photo-1.jpg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '1994',
@@ -130,8 +117,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Roxanne was 15 years old when she disappeared from Prince George on a long weekend in July.',
 		link: '/roxanne-thiara',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/roxanne-thiara-photo.jpeg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/roxanne-thiara-photo.jpeg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '1994',
@@ -140,8 +126,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'15-year-old Alishia Germaine was last seen leaving a Christmas party in Prince Rupert.',
 		link: '/alishia-germaine',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/alishia-germaine.jpg?fit=543%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/alishia-germaine.jpg?fit=543%2C611&ssl=1`,
 	},
 	{
 		year: '1995',
@@ -150,8 +135,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Lana Patricia Derrick came home from college for Thanksgiving, but mysteriously disappeared after a night out.',
 		link: '/lana-derrick',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/lana-derrick.jpg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/lana-derrick.jpg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '2002',
@@ -159,8 +143,7 @@ export const TIMELINE_DATA = [
 		name: 'Nicole Hoar',
 		blurb:
 			'Nicole was working as a tree planter and was last seen at a gas station on Highway 16, hitchhiking from Prince George to Smithers.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/nicole-hoar.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/nicole-hoar.jpeg`,
 	},
 	{
 		year: '2004',
@@ -168,8 +151,7 @@ export const TIMELINE_DATA = [
 		name: 'Kayla Rose McKay',
 		blurb:
 			'Kayla was 13 years old when she went missing from Prince Rupert. Her body was found two days later near train tracks on a waterfront on George Hills Way.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/kayla-mckay.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/kayla-mckay.jpeg`,
 	},
 	{
 		year: '2004',
@@ -177,8 +159,7 @@ export const TIMELINE_DATA = [
 		name: 'Margaret Nooski',
 		blurb:
 			'Maragret was 89 years old when she went missing and was last seen hitchhiking on Highway 16. Police believe she may have been trying to get to Prince George, or headed into the bush in search of trapping territory.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/unknown-image.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/unknown-image.png`,
 	},
 	{
 		year: '2005',
@@ -187,8 +168,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Tamara was a young mother who disappeared after hitchhiking from Prince Rupert to Terrace in 2015.',
 		link: '/tamara-chipman',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/tamara-chipman-photo.jpg?fit=656%2C611&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/tamara-chipman-photo.jpg?fit=656%2C611&ssl=1`,
 	},
 	{
 		year: '2005',
@@ -196,8 +176,7 @@ export const TIMELINE_DATA = [
 		name: 'Mary Madeline George',
 		blurb:
 			'Mary was last seen in the evening of July 24, 2005 at 6pm. She was walking from Ospika Blvd. to the Spruceland Mall Walk-in Clinic.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/mary-madeline-george.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/mary-madeline-george.jpeg`,
 	},
 	{
 		year: '2006',
@@ -206,8 +185,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Aielah Saric-Auger was 14 years old when she went missing and was last seen along Highway 16.',
 		link: '/aielah-saric-auger',
-		photo:
-			'https://i0.wp.com/wordpress.thehueandcry.com/wp-content/uploads/aielah-auger.jpg?fit=1200%2C722&ssl=1',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}//aielah-auger.jpg?fit=1200%2C722&ssl=1`,
 	},
 	{
 		year: '2007',
@@ -215,8 +193,7 @@ export const TIMELINE_DATA = [
 		name: 'Bonnie Marie Joseph',
 		blurb:
 			'Bonnie was a mother of five and was last seen hitchhiking from Vanderhoof to Prince George to attend a court date. She missed the appointment and was not heard from again. Her wallet, ID, and an uncashed cheque were found near a lake.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/bonnie-joseph.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/bonnie-joseph.png`,
 	},
 	{
 		year: '2010',
@@ -224,8 +201,7 @@ export const TIMELINE_DATA = [
 		name: 'Emmalee Rose Mclean',
 		blurb:
 			'Emma was 16 years old when she was last seen out with a group of people the night before she went missing. Her body was discovered along the Prince Rupert waterfront.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/emma-rose.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/emma-rose.png`,
 	},
 	{
 		year: '2013',
@@ -234,8 +210,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Mackie was last seen at a party in a cabin with two men before she went missing.',
 		link: '/immaculate-basil',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/Immaculate_Basil.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Immaculate_Basil.jpeg`,
 	},
 	{
 		year: '2014',
@@ -244,7 +219,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Anita was last seen at a gas station, her car was found the next day near Highway 16.',
 		link: '/anita-florence-thorne',
-		photo: 'https://wordpress.thehueandcry.com/wp-content/uploads/anita-t.jpg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/anita-t.jpg`,
 	},
 	{
 		year: '2017',
@@ -252,8 +227,7 @@ export const TIMELINE_DATA = [
 		name: 'Frances Brown',
 		blurb:
 			'Frances was last seen picking mushrooms with a companion, an activity she did every year. She was raised by a trapper and an experienced back country explorer, which made this disappearance unusual. Despite search efforts she was never seen again.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/frances-brown.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/frances-brown.jpeg`,
 	},
 	{
 		year: '2018',
@@ -261,8 +235,7 @@ export const TIMELINE_DATA = [
 		name: 'Chantelle Catherine Simpson',
 		blurb:
 			'Chantelle was 34 years old when she went missing after leaving her home on July 4th. Her abandoned car was found next to a gravel pit the next day. Days later her body was found in the Skeena River by a train conductor who was passing by.',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/chantel-image.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/chantel-image.png`,
 	},
 	{
 		year: '2018',
@@ -271,8 +244,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			'Jessica was 18 years old when she dropped off her daughter with her grandmother for the night but never returned to pick her up.',
 		link: '/jessica-patrick-balczer',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/Jessica-Patrick-photo.jpeg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Jessica-Patrick-photo.jpeg`,
 	},
 	{
 		year: '2018',
@@ -281,8 +253,7 @@ export const TIMELINE_DATA = [
 		blurb:
 			"Cynthia was last seen leaving her mother's house one evening. She was never seen or heard from again.",
 		link: '/cynthia-martin',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/cynthia-martin.png',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/cynthia-martin.png`,
 	},
 	{
 		year: '2023',
@@ -291,7 +262,6 @@ export const TIMELINE_DATA = [
 		blurb:
 			"Chelsey was 29 years old and was last seen leaving her father's house in the early morning. She never returned home.",
 		link: '/chelsey-quaw',
-		photo:
-			'https://wordpress.thehueandcry.com/wp-content/uploads/chelsey-photo.jpg',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/chelsey-photo.jpg`,
 	},
 ]
