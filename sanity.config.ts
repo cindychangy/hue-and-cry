a0db2eecebb9b054559f69c55f7090c6360c6b02
Cindy Chang <cindyisachang@gmail.com>
2024-09-28 09:33:47 +0200

    More changes

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
-  name: 'hueandcry',
-  title: 'hue-and-cry',
-  projectId: 'i6ujk2hj',
-  dataset: 'production',
-  basePath: "/studio",
+	name: 'hueandcry',
+	title: 'hue-and-cry',
+	projectId: 'i6ujk2hj',
+	dataset: 'production',
+	basePath: '/studio',
 
-  plugins: [structureTool(), visionTool()],
+	plugins: [structureTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
+	schema: {
+		types: schemaTypes,
+	},
 })
