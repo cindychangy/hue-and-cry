88415dd520f22380370e0a2f79bf3c50a4df07cd
Cindy Chang <cindyisachang@gmail.com>
2024-09-28 09:46:32 +0200

    Add constants

---
+++ constants/timeline.tsx
@@ -0,0 +1,267 @@
+export const TIMELINE_DATA = [
+	{
+		year: '1969',
+		location: 'Williams Lake',
+		name: 'Gloria Moody',
+		blurb:
+			'Gloria went out one night with her brother only to disappear behind him on their way home.',
+		link: '/gloria-moody',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/gloria-moody.jpeg`,
+	},
+	{
+		year: '1970',
+		location: "Hudson's Hope",
+		name: 'Micheline Pare',
+		blurb: 'Micheline was 18 years old when she went missing along Highway 16.',
+		link: '/micheline-pare',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/micheline-pare.png`,
+	},
+	{
+		year: '1970',
+		location: 'Prince George',
+		name: 'Helen Claire Frost',
+		blurb:
+			'Helen disappeared just four days before her 18th birthday. She was last seen leaving the apartment she shared with her sister, telling her sister she wanted to go for a quick walk. She never returned home.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/helen-claire-frost.jpeg`,
+	},
+	{
+		year: '1971',
+		location: 'Gitsegukla',
+		name: 'Jean Virginia Sampare',
+		blurb:
+			'Jean was last seen hanging out with her cousin on the railroad overpass on Highway 16.',
+		link: '/jean-virginia-sampare',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Jean_Virginia_Sampare.png`,
+	},
+	{
+		year: '1973',
+		location: 'Clearwater',
+		name: 'Gale Weys',
+		blurb:
+			"Gale Weys was 19 years old when she was last seen trying to get a ride to her parent's house.",
+		link: '/gale-weys',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/gale-weys-photo.jpg`,
+	},
+	{
+		year: '1973',
+		location: 'Kamloops',
+		name: 'Pamela Darlington',
+		blurb:
+			'Pamela was 19 years old when she went missing, she was last seen in a bar in Kamloops. Her body was found the next day in a park. She had been sexually assaulted and beaten to death.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/pamela-photo.jpg`,
+	},
+	{
+		year: '1974',
+		location: 'Thornhill',
+		name: 'Monica Ignas',
+		blurb:
+			'Monica was 14 years old and was last seen walking along Highway 16 in the direction of her house.',
+		link: '/monica-ignas',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/monica-ignas.png`,
+	},
+	{
+		year: '1974',
+		location: 'Lac la Hache',
+		name: 'Colleen MacMillen',
+		blurb:
+			"Colleen was 16 years old when she disappeared hitchhiking to a friend's house. Her remains were found one month later. In 2012, with advancements in DNA technology the RCMP announced Bobby Jack Fowler as a suspect in her killing. ",
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/colleen-photo.jpg`,
+	},
+	{
+		year: '1978',
+		location: 'Prince Rupert',
+		name: 'Mary Jane Hill',
+		blurb:
+			'Mary Jane Hill was 31 years old and a mother of three. Her body was found nude along Highway 16 and her death was deemed to be from bronchitis and bronchopneumonia as a result of manslaughter. ',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/mary-jane-hill.png`,
+	},
+	{
+		year: '1989',
+		location: 'Prince George',
+		name: 'The Jack Family',
+		blurb:
+			'Doreen Jack and her husband Ronald were offered jobs at a logging camp from an unknown man. The struggling family of four accepted the job and ride the man offered. The family was last seen at 1:20am that night leaving with the man in his car.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/doreen-jack.jpeg`,
+	},
+	{
+		year: '1989',
+		location: 'Prince Rupert',
+		name: 'Alberta Williams',
+		blurb:
+			'Alberta Williams was 24 years old when she was last seen leaving a bar in Prince Rupert.',
+		link: '/alberta-williams',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/alberta-williams-photo.jpg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '1990',
+		location: 'Smithers',
+		name: 'Delphine Nikal',
+		blurb:
+			'Delphine Nikal was last seen leaving her group of friends one night to head home, but never made it back.',
+		link: '/delphine-nikal',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/delphine-nikal-poster.jpg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '1994',
+		location: 'Smithers',
+		name: 'Ramona Wilson',
+		blurb:
+			'Ramona Wilson was last seen on a Saturday night leaving her house to meet friends for a night out. No one has seen her since.',
+		link: '/ramona-wilson',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/ramona-wilson-photo-1.jpg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '1994',
+		location: 'Prince George',
+		name: 'Roxanne Thiara',
+		blurb:
+			'Roxanne was 15 years old when she disappeared from Prince George on a long weekend in July.',
+		link: '/roxanne-thiara',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/roxanne-thiara-photo.jpeg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '1994',
+		location: 'Prince George',
+		name: 'Alisha Germaine',
+		blurb:
+			'15-year-old Alishia Germaine was last seen leaving a Christmas party in Prince Rupert.',
+		link: '/alishia-germaine',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/alishia-germaine.jpg?fit=543%2C611&ssl=1`,
+	},
+	{
+		year: '1995',
+		location: 'Terrace',
+		name: 'Lana Patricia Derrick',
+		blurb:
+			'Lana Patricia Derrick came home from college for Thanksgiving, but mysteriously disappeared after a night out.',
+		link: '/lana-derrick',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/lana-derrick.jpg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '2002',
+		location: 'Prince George',
+		name: 'Nicole Hoar',
+		blurb:
+			'Nicole was working as a tree planter and was last seen at a gas station on Highway 16, hitchhiking from Prince George to Smithers.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/nicole-hoar.jpeg`,
+	},
+	{
+		year: '2004',
+		location: 'Prince Rupert',
+		name: 'Kayla Rose McKay',
+		blurb:
+			'Kayla was 13 years old when she went missing from Prince Rupert. Her body was found two days later near train tracks on a waterfront on George Hills Way.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/kayla-mckay.jpeg`,
+	},
+	{
+		year: '2004',
+		location: 'Fraser Lake',
+		name: 'Margaret Nooski',
+		blurb:
+			'Maragret was 89 years old when she went missing and was last seen hitchhiking on Highway 16. Police believe she may have been trying to get to Prince George, or headed into the bush in search of trapping territory.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/unknown-image.png`,
+	},
+	{
+		year: '2005',
+		location: 'Prince Rupert',
+		name: 'Tamara Chipman',
+		blurb:
+			'Tamara was a young mother who disappeared after hitchhiking from Prince Rupert to Terrace in 2015.',
+		link: '/tamara-chipman',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/tamara-chipman-photo.jpg?fit=656%2C611&ssl=1`,
+	},
+	{
+		year: '2005',
+		location: 'Prince George',
+		name: 'Mary Madeline George',
+		blurb:
+			'Mary was last seen in the evening of July 24, 2005 at 6pm. She was walking from Ospika Blvd. to the Spruceland Mall Walk-in Clinic.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/mary-madeline-george.jpeg`,
+	},
+	{
+		year: '2006',
+		location: 'Prince George',
+		name: 'Aielah Katerina Saric-Auger',
+		blurb:
+			'Aielah Saric-Auger was 14 years old when she went missing and was last seen along Highway 16.',
+		link: '/aielah-saric-auger',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}//aielah-auger.jpg?fit=1200%2C722&ssl=1`,
+	},
+	{
+		year: '2007',
+		location: 'Vanderhoof',
+		name: 'Bonnie Marie Joseph',
+		blurb:
+			'Bonnie was a mother of five and was last seen hitchhiking from Vanderhoof to Prince George to attend a court date. She missed the appointment and was not heard from again. Her wallet, ID, and an uncashed cheque were found near a lake.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/bonnie-joseph.png`,
+	},
+	{
+		year: '2010',
+		location: 'Prince Rupert',
+		name: 'Emmalee Rose Mclean',
+		blurb:
+			'Emma was 16 years old when she was last seen out with a group of people the night before she went missing. Her body was discovered along the Prince Rupert waterfront.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/emma-rose.png`,
+	},
+	{
+		year: '2013',
+		location: 'Tachie',
+		name: 'Immaculate "Mackie" Basil',
+		blurb:
+			'Mackie was last seen at a party in a cabin with two men before she went missing.',
+		link: '/immaculate-basil',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Immaculate_Basil.jpeg`,
+	},
+	{
+		year: '2014',
+		location: 'Prince George',
+		name: 'Anita Florence Thorne',
+		blurb:
+			'Anita was last seen at a gas station, her car was found the next day near Highway 16.',
+		link: '/anita-florence-thorne',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/anita-t.jpg`,
+	},
+	{
+		year: '2017',
+		location: 'Smithers',
+		name: 'Frances Brown',
+		blurb:
+			'Frances was last seen picking mushrooms with a companion, an activity she did every year. She was raised by a trapper and an experienced back country explorer, which made this disappearance unusual. Despite search efforts she was never seen again.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/frances-brown.jpeg`,
+	},
+	{
+		year: '2018',
+		location: 'Terrace',
+		name: 'Chantelle Catherine Simpson',
+		blurb:
+			'Chantelle was 34 years old when she went missing after leaving her home on July 4th. Her abandoned car was found next to a gravel pit the next day. Days later her body was found in the Skeena River by a train conductor who was passing by.',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/chantel-image.png`,
+	},
+	{
+		year: '2018',
+		location: 'Smithers',
+		name: 'Jessica Patrick Balczer',
+		blurb:
+			'Jessica was 18 years old when she dropped off her daughter with her grandmother for the night but never returned to pick her up.',
+		link: '/jessica-patrick-balczer',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/Jessica-Patrick-photo.jpeg`,
+	},
+	{
+		year: '2018',
+		location: 'New Hazelton',
+		name: 'Cynthia Martin',
+		blurb:
+			"Cynthia was last seen leaving her mother's house one evening. She was never seen or heard from again.",
+		link: '/cynthia-martin',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/cynthia-martin.png`,
+	},
+	{
+		year: '2023',
+		location: 'Vanderhoof',
+		name: 'Chelsey Quaw',
+		blurb:
+			"Chelsey was 29 years old and was last seen leaving her father's house in the early morning. She never returned home.",
+		link: '/chelsey-quaw',
+		photo: `${process.env.NEXT_PUBLIC_MEDIA_URL}/chelsey-photo.jpg`,
+	},
+]
