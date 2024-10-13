fba261ae52bca981800670e88e02c632206408d2
Cindy Chang <cindyisachang@gmail.com>
2024-10-13 09:18:56 +0200

    Mods

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -3,14 +3,14 @@ import { dataset, projectId } from './sanity.api';
 import type { Image } from 'sanity';
 
 const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
+	projectId: projectId || '',
+	dataset: dataset || '',
 });
 
 export const urlForImage = (source: Image) => {
-  if (!source?.asset?._ref) {
-    return '';
-  }
+	if (!source?.asset?._ref) {
+		return '';
+	}
 
-  return imageBuilder.image(source).auto('format').fit('max').url();
+	return imageBuilder.image(source).auto('format').fit('max').url();
 };
