87eddf134d9a303b5e4f8950a8bfc2094e2713f6
Cindy Chang <cindyisachang@gmail.com>
2026-02-01 18:17:04 +0100

    Update date

--- components/SiteFooter/SiteFooter.tsx
+++ components/SiteFooter/SiteFooter.tsx
@@ -8,7 +8,7 @@ export const SiteFooter = () => {
 		<div className={styles.siteFooter}>
 			<div className={styles.footerContent}>
 				<p className={styles.copyright}>
-					© 2025 Hue and Cry. All Rights Reserved.
+					© {new Date().getFullYear()} Hue and Cry. All Rights Reserved.
 				</p>
 				<ul className={styles.navigation}>
 					{NAV_LINKS.map((item) => (
